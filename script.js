1f416a5a502a7261fb8d13b8200423b25a7ccaaf
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-04-21 02:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const msg = document.getElementById('text');
+const delay = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const displaymsg = document.getElementById('output');
+
+
+function a(){
+ 
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve(`<p>${msg.value}</p>`);
+    },delay.value)
+  });
+}
+
+async function displayMsg(){
+  const result = await a().then((el)=>displaymsg.innerHTML=el);
+}
+
+btn.addEventListener('click',displayMsg);
